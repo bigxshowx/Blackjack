b5743c61fabc1be4c4be4127447b496f6c8544f8
bigxshowx <bigxshowx@gmail.com>
2023-05-22 20:24:29 +0900

    commit

--- blackjack.py
+++ blackjack.py
@@ -1,3 +1,27 @@
-from packages import draw
+from packages import *
 
-print(draw())
+'''Need classes for a deck, human player, bankroll for bets:
+1- Dealer 1 up and one down for self, player 2 cards up
+2- Actions to stay or hit only, ignore others (split...)
+3- each hit sum the card vs. dealer..
+4- dealer then hits until
+5 - win (money goes to player), push, or bust (money goes to dealer..)
+'''
+
+new_card = Card("Hearts","Ace")
+
+def ace_dealt(card):
+    if card.rank == "Ace":
+        choice = None #input('Choose value 1 or 11')
+        return choice
+
+print(new_card)
+print(new_card.value)
+
+testing = ace_dealt(new_card)
+
+print(testing)
+
+new_deck = Deck()
+
+print(len(new_deck.all_cards))
--- packages/__init__.py
+++ packages/__init__.py
@@ -1,5 +1,45 @@
 import random
 
-def draw():
-    return random.random()
+values = {"Two":2, "Three":3, "Four":4, "Five":5, "Six":6, "Seven":7,
+          "Eight":8, "Nine":9, "Ten":10, "Jack":10, "Queen":10, "King":10, "Ace":(1,11)}
+
+suits = ('Hearts', 'Diamonds', 'Spades', 'Clubs')
+
+ranks = ("Two", "Three", "Four", "Five", "Six", "Seven",
+          "Eight", "Nine", "Ten", "Jack", "Queen", "King", "Ace")
+
+class Card():
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+        self.value = values[rank]
+
+    def __str__(self):
+        return self.rank + " of " + self.suit
+
+class Deck():
+    def __init__(self):
+        self.all_cards = []
+
+        for suit in suits:
+            for rank in ranks:
+                created_card = Card(suit,rank)
+                self.all_cards.append(created_card)
+
+    def shuffle_deck(self):
+        random.shuffle(self.all_cards)
+
+    def deal_one(self):
+        return self.all_cards.pop()
+
+
+class Bankroll():
+
+    def __init__(self, amount):
+        self.balance = 100
+        self.amount = amount
+
+    def deposit(self):
+        self.balance += amount
+
 
